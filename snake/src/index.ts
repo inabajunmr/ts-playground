bf5cb433c585c1a5123044b0f85e38fe37c56175
inabajunmr <inabajun.for.regi@gmail.com>
2020-12-21 01:27:12 +0900

    Game class

--- snake/src/index.ts
+++ snake/src/index.ts
@@ -1,4 +1,49 @@
-const a = {};
-let b = {};
-console.log('aaa');
-alert('aaa');
+class Game {
+    private maxX: number;
+    private maxY: number;
+    private cells: Array<Array<boolean>>;
+
+    constructor(maxX: number, maxY: number) {
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.cells = new Array<Array<boolean>>(maxX);
+        this.cells[0] = new Array();
+        for (let index = 0; index < this.cells.length; index++) {
+            this.cells[index] = new Array(maxY).fill(false);
+        }
+    }
+
+    async flip() {
+        this.cells[0][0] = !this.cells[0][0];
+    }
+
+    async sleep(ms: number) {
+        return new Promise((r) => setTimeout(r, ms));
+    }
+
+    async start() {
+        while (true) {
+            await this.sleep(100);
+            await this.flip();
+            await this.print();
+        }
+    }
+
+    async print() {
+        const a = document.getElementById('game');
+        let html = '';
+        this.cells.forEach((x) => {
+            x.forEach((y) => {
+                if (y) {
+                    html += '■';
+                } else {
+                    html += '□';
+                }
+            });
+            html += '<br>';
+        });
+        a!.innerHTML = html;
+    }
+}
+
+new Game(20, 20).start();
